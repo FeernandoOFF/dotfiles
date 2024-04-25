98d6b102fe0dbb885969daf6a70eb1ec6fbf0c58
Fernando Obregon <fernandoobregon@fernandos-mbp.home>
2024-04-26 00:39:22 +0100

    [Updated] ZSH file

--- scripts/terminal_apps.sh
+++ scripts/terminal_apps.sh
@@ -1,4 +1,4 @@
 #bin/bash
-brw install neovim && brew install ranger && brew install tmux && asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git &&
+brew install neovim && brew install ranger && brew install tmux && asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git &&
 	ln -s ~/dotfiles/nvim ~/.config/nvim/ &&
 	ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
