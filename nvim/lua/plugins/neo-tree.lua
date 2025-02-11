b49f0dcc6636e2aaf2d73abc452ec108a0d79041
Fernando Obregon <fernandoobregon@mac.lan>
2025-02-11 15:47:09 +0000

    [Added] NeoTree on right-side
    Minor :: Added Svelte and Kotlin languages for tree sitter

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,32 +4,29 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
-      { "<leader>E", "<cmd>Neotree reveal<cr>", desc = "Neotree reveal current fle" },
-    },
-    opts = {
-      event_handlers = {
-
-        {
-          event = "file_opened",
-          handler = function(file_path)
-            require("neo-tree.command").execute({ action = "close" })
-          end,
-        },
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            source = "filesystem",
+            position = "right",
+          })
+        end,
+        desc = "Neotree reveal current fle",
+      },
+      {
+        "<leader>E",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            source = LazyVim.root(),
+            position = "right",
+          })
+        end,
+        desc = "Neotree reveal current fle",
       },
     },
-    config = function()
-      require("neo-tree").setup({
-        filesystem = {
-          group_empty_dirs = true,
-        },
-        buffers = {
-          group_empty_dirs = true,
-        },
-      })
-
-      vim.g.nvim_tree_group_empty = 1
-
-      require("neo-tree.command").execute({ action = "close" })
-    end,
+    opts = {},
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -38,6 +38,8 @@ return {
         "tsx",
         "typescript",
         "astro",
+        "svelte",
+        "kotlin",
       })
     end,
   },
