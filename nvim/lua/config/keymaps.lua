e8ff616e8909ecb7646c57accc0afd5298ac52c3
Fernando Obregon <fernandoobregon@mac.lan>
2025-02-13 10:33:17 +0000

    [Updated] Neovim background to be transparent and added keybindings to move around the code

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,7 +4,12 @@ local lazyterm = function()
   Util.terminal(nil, { cwd = Util.root() })
 end
 
+-- Save file with space w
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+-- Pane management
+
+vim.keymap.set("n", "<C-w>", "<cmd>vsplit<cr>", { desc = "Split window right" })
+
 vim.keymap.set("n", "<leader>2", lazyterm, { desc = "Terminal (root dir)" })
 
 vim.keymap.set("n", "<leader>p", function(command, opts)
@@ -16,43 +21,48 @@ end, {
 vim.keymap.set("n", "<leader>P", function()
   Snacks.picker.keymaps()
 end, { desc = "Keymaps" })
+-- MOVEMENT  & Navigation
 
--- LSP actions
+-- Centering on movement
+-- vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "N", "Nzzzv")
+-- vim.keymap.set("n", "n", "nzzzv")
+
+-- Code Actions
 -- vim.keymap.set("n", "gi", "<Cmd>Telescope lsp_references<CR>", { desc = "Go to usage" })
 -- vim.keymap.set("n", "cd", "<Cmd>lua vim.lsp.buf.rename()<CR>", { desc = "Rename" })
 
--- Centering on movement
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "n", "nzzzv")
+-- Move selected block on visual mode
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true })
 
--- greatest remap ever
+-- Indent with Tab and de-indent with Shift+Tab in visual mode
+vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent selected text" })
+vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "De-indent selected text" })
+
+-- Paste & Delete?
 -- vim.keymap.set("x", "<leader>p", [["_dP]])
 -- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { silent = true })
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { silent = true })
-
 -- Tmux workspaces
 -- local workspace = require("workspace")
 -- vim.keymap.set("n", "<C-f>", workspace.tmux_sessions)
 
 -- Jump lists
-
 vim.keymap.set("n", "gb", "<C-o>", { desc = "Jump to previous location in jumplist" })
 
+-- Set jumplist when moveing more than 3 lines
 vim.keymap.set(
   "n",
   "j",
   [[v:count ? (v:count >= 3 ? "m'" . v:count : '') . 'j' : 'gj']],
   { noremap = true, expr = true }
 )
-
 vim.keymap.set(
   "n",
   "k",
