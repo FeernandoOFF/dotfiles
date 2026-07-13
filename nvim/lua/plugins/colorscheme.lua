2e9e4ff3d810f4c63452a4969e7f2f0a0cb74937
Fernando <fernandoobregon@tapadoo.com>
2026-07-13 11:31:59 +0100

    style(nvim): improved contrast for transparent background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,19 @@ return {
         sidebars = "transparent",
         floats = "transparent",
       },
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = c.dark5 }
+        hl.LineNrAbove = { fg = c.dark5 }
+        hl.LineNrBelow = { fg = c.dark5 }
+        hl.CursorLineNr = { fg = c.orange, bold = true }
+        hl.SnacksPickerFile = { fg = c.fg }
+        hl.SnacksPickerDirectory = { fg = c.blue, bold = true }
+        hl.SnacksPickerPathHidden = { fg = c.fg_dark }
+        hl.SnacksPickerPathIgnored = { fg = c.dark5 }
+        hl.SnacksPickerGitStatus = { fg = c.orange }
+        hl.SnacksPickerGitStatusUntracked = { fg = c.orange }
+        hl.Directory = { fg = c.blue, bold = true }
+      end,
     },
   },
 }
