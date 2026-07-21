1b63d18cd105e72c97f903f0fe254039c2b9c8f3
Fernando <fernandoobregon@tapadoo.com>
2026-07-21 15:13:15 +0100

    feat(setup): updated brew install script

--- scripts/terminal_apps.sh
+++ scripts/terminal_apps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-brew install neovim ranger tmux mise &&
-	mise use -g node@latest   # nodejs via mise (config symlinks are handled by `make stow`)
+brew install neovim mise television fd ripgrep bat &&
+  mise use -g node@latest # nodejs via mise (config symlinks are handled by `make stow`)
